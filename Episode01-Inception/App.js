81ea1ece45ebe230adf8d091447f24e91d625c6b
ganesh1172 <ganesh172595@gmail.com>
2023-09-07 18:05:59 +0530

    code complex structure using react cdn"

---
+++ Episode01-Inception/App.js
@@ -0,0 +1,18 @@
+const heading = React.createElement("div", { id: "heading" }, [
+  React.createElement("h3", {}, "Heading React1"),
+  React.createElement(
+    "div",
+    { id: "sub-heading1" },
+    React.createElement("h3", {}, "Hello World from React1"),
+    React.createElement("h3", {}, "Hello World from React2")
+  ),
+  React.createElement("h3", {}, "Heading React2"),
+  React.createElement(
+    "div",
+    { id: "sub-heading2" },
+    React.createElement("h3", {}, "Hello World from React1"),
+    React.createElement("h3", {}, "Hello World from React2")
+  ),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
