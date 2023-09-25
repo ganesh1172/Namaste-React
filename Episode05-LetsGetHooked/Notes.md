189fc0b7ffba99bff8d9ffef6d85ef47ba7f6b14
ganesh1172 <ganesh172595@gmail.com>
2023-09-25 20:52:03 +0530

    finished episode 05 - Lets Get Hooked

--- Episode05-LetsGetHooked/Notes.md
+++ Episode05-LetsGetHooked/Notes.md
@@ -15,3 +15,36 @@
 * for multiple things from single file
 * use { fileName } to import things
 * import {component} from "path
+
+### Hooked
+
+- Normal JS utility functions
+
+* Two Important Hooks:-
+
+- useState() - Superpowerful state variables
+- useEffect()
+
+* useState():
+
+- To use in file, import from react. Write variable inside component.
+- whenever state variable react will re-render component
+
+- Event-Handler
+
+* pass attribute on button for e.g. onClick={() => {console.log('test');}}
+
+* State Variable - super powerful variable
+
+##### Virtual DOM(V-DOM)
+
+- It is representation of actual DOM
+
+##### Reconciliation Algorithm/ React Fibre
+
+- react-fibre new way of finding diff and updating dom
+- acdlite/react-fibre-architecture
+
+##### Diff Algorithm
+
+- Find the difference between old V-DOM and new V-DOM
--- Episode05-LetsGetHooked/src/components/Body.js
+++ Episode05-LetsGetHooked/src/components/Body.js
@@ -1,12 +1,27 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurants, useListOfRestaurants] = useState(resList);
+
   return (
     <div className="body">
-      <div className="search">SearchBar</div>
+      <div className="filter">
+        <button
+          className="btn-filter"
+          onClick={() => {
+            const filteredList = resList.filter(
+              (res) => res.data.avgRating > 4
+            );
+            useListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.data.id} resData={restaurant} />
         ))}
       </div>
